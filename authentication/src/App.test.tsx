9aec5b30ded52e2b88f9ae218fe4f14441bfe447
Prince Thomas <pthomas@aptean.com>
2022-04-20 15:44:54 +0530

    feat(authentication): add test case

--- authentication/src/App.test.tsx
+++ authentication/src/App.test.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { RecoilRoot } from "recoil";
-import { screen, render } from "@testing-library/react";
-import '@testing-library/jest-dom'
+import { screen, render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "./App";
+
+jest.mock("react-google-login", () => {
+  return {
+    GoogleLogin: (props: any) => {
+      const onClick = () => {
+        props.onSuccess({
+          profileObj: {
+            tokenId: "idds",
+            name: "John",
+          },
+        });
+      };
+      return <button onClick={onClick} data-testid="google-button" />;
+    },
+  };
+});
 
 test("Test all the texts are present on screen", () => {
   render(
@@ -11,4 +27,23 @@ test("Test all the texts are present on screen", () => {
     </RecoilRoot>
   );
   expect(screen.getByText("PollApp")).toBeInTheDocument();
+  expect(screen.getByText("Create your own polls.")).toBeInTheDocument();
+  expect(
+    screen.getByText("Login to create your own poll.")
+  ).toBeInTheDocument();
+});
+
+test("Test sign in works", () => {
+  render(
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  );
+  expect(
+    screen.getByText("Login to create your own poll.")
+  ).toBeInTheDocument();
+  fireEvent.click(screen.getByTestId("google-button"));
+  expect(
+    screen.getByText("Hi John, go to profile to create poll")
+  ).toBeInTheDocument();
 });
