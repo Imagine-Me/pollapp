808cdfb1f8d804c5197b3cbc15b7337efdd27160
Prince <princekizhthara@gmail.com>
2022-06-08 15:18:17 +0530

    feat(poll): add header in the poll

--- poll/src/components/Poll.tsx
+++ poll/src/components/Poll.tsx
@@ -1,9 +1,12 @@
-import { Card, Col, RadioChangeEvent, Row } from "antd";
+import { Card, Col, RadioChangeEvent, Row, Typography } from "antd";
 import React from "react";
 import styled from "styled-components";
-import { QuestionInterface } from "../common.interface";
+import { useRecoilValue } from "recoil";
 import QuestionComponent from "./Questions";
 import ChartComponent from "./Chart/Chart";
+import { data } from "../recoil/data";
+
+const { Title } = Typography;
 
 const RowStyled = styled(Row)`
   min-height: 100vh;
@@ -20,6 +23,17 @@ const QuestionContent = styled((props) => <div {...props} />)`
   flex-flow: column;
   > div:first-child {
     flex-grow: 1;
+  }
+`;
+
+const HeaderDiv = styled(Row)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 15px;
+  > h3, h2 {
+    margin: 0 !important;
   }
 `;
 
@@ -40,33 +54,45 @@ const PollContent = ({
   selectOption,
   questionLegend,
 }: Props) => {
+  const { title, userCount } = useRecoilValue(data);
+
   const onSelectOption = (e: RadioChangeEvent) => {
     if (selectOption && e.target.value !== undefined) {
       selectOption(e.target.value);
     }
   };
   return (
-    <RowStyled align="middle">
-      {showChart && (
-        <Col xs={10}>
-          <ChartComponent />
+    <>
+      <HeaderDiv align="middle" justify="space-between">
+        <Title level={2}>
+          {`>`} {title}
+        </Title>
+        <Title level={3}>
+          Users: {userCount}
+        </Title>
+      </HeaderDiv>
+      <RowStyled align="middle">
+        {showChart && (
+          <Col xs={10}>
+            <ChartComponent />
+          </Col>
+        )}
+        <Col flex="auto">
+          <CardStyled>
+            <QuestionContent>
+              <QuestionComponent
+                isHost={isHost}
+                onSelectOption={onSelectOption}
+                value={answer}
+                questionLegend={questionLegend}
+                showSelected={showChart}
+              />
+              {footer}
+            </QuestionContent>
+          </CardStyled>
         </Col>
-      )}
-      <Col flex="auto">
-        <CardStyled>
-          <QuestionContent>
-            <QuestionComponent
-              isHost={isHost}
-              onSelectOption={onSelectOption}
-              value={answer}
-              questionLegend={questionLegend}
-              showSelected={showChart}
-            />
-            {footer}
-          </QuestionContent>
-        </CardStyled>
-      </Col>
-    </RowStyled>
+      </RowStyled>
+    </>
   );
 };
 
