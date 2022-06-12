94fe4401d0fc27c2e91c22e1bf6f7400a995d40c
Prince <princekizhthara@gmail.com>
2022-06-12 10:29:25 +0530

    feat(poll): add alert for poll

--- poll/src/components/Chart/Chart.tsx
+++ poll/src/components/Chart/Chart.tsx
@@ -1,19 +1,34 @@
 import React, { useState } from "react";
-import { Col, Row, Select } from "antd";
+import { Alert, Col, Row, Select } from "antd";
 import BarChart from "./Bar";
 import Title from "antd/lib/typography/Title";
 import { useRecoilValue } from "recoil";
 import { data } from "../../recoil/data";
+import styled from "styled-components";
+import { joinUserData } from "../../recoil/join";
 
 const { Option } = Select;
 
 export type ChartType = "bar" | "pie";
 
+const AlertStyled = styled(Alert)`
+  max-width: 300px;
+  margin: auto;
+`;
+
 const ChartComponent = () => {
   const [chartType, setChartType] = useState<ChartType>("bar");
   const { userCount, question } = useRecoilValue(data);
-  console.log(question.poll);
+  const { answer, isPolled } = useRecoilValue(joinUserData);
   const totalPolled = question.poll?.reduce((acc, val) => acc + val, 0);
+  let alert = null;
+  if (question.answer !== undefined && answer && isPolled) {
+    if (question.answer === answer) {
+      alert = <AlertStyled message="Correct" type="success" showIcon />;
+    } else {
+      alert = <AlertStyled message="Wrong" type="error" showIcon />;
+    }
+  }
   return (
     <Row>
       <Col span={12}>
@@ -30,6 +45,7 @@ const ChartComponent = () => {
       </Col>
       <Col span={24}>
         <BarChart />
+        {alert}
       </Col>
     </Row>
   );
