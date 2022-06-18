19bdebdba05d4aebebcbb125b52c03c8ed17d27e
Prince <princekizhthara@gmail.com>
2022-06-18 15:16:19 +0530

    refactor(profile): refactor app

--- profile/src/App.tsx
+++ profile/src/App.tsx
@@ -1,18 +1,43 @@
 import React from "react";
-import { Layout } from "antd";
+import { Button, Layout, Typography } from "antd";
 import { Routes, Route } from "react-router-dom";
-import UserPage from "./pages/User";
+import Polls from "./components/Polls/Polls";
+import Questions from "./components/Questions/Question";
+import ContentStyled from "./components/Content";
+import styled from "styled-components";
 
-if (process.env.NODE_ENV) {
+if (location.host === process.env.APP_URL) {
   import("./App.css");
 }
 
+const { Header } = Layout;
+const { Title } = Typography;
+
+const HeaderStyled = styled(Header)`
+  background-color: white !important;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 30px !important;
+`;
+
 const App = () => {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/*" element={<UserPage />} />
-      </Routes>
+    <Layout style={{ height: "100%" }}>
+      <HeaderStyled>
+        <Title level={3} style={{ lineHeight: "inherit", margin: 0 }}>
+          PollApp
+        </Title>
+        <Button type="text" danger>
+          Logout
+        </Button>
+      </HeaderStyled>
+      <ContentStyled>
+        <Routes>
+          <Route path="/polls" element={<Polls />} />
+          <Route path="/polls/:pollId" element={<Questions />} />
+        </Routes>
+      </ContentStyled>
     </Layout>
   );
 };
